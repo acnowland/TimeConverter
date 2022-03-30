17c6b300d3631959d78d34305c11ec0ce8accd0d
Adam Nowland <acnowland@gmail.com>
2022-03-29 21:01:02 -0700

    Initial Commit

---
+++ TimeConverter/TimeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeConverterApp.swift
+//  TimeConverter
+//
+//  Created by Adam Nowland on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
